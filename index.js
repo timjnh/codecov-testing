1dfa8e7bfc3acc76e8fd82c96f40f909abd14b08
Tim_Jones <tjones@aurorasolar.com>
2023-04-13 12:43:04 -0400

    Adding subtraction and untested code

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
-import { addNumbers } from './numbers.js';
+import { addNumbers, subtractNumbers } from './numbers.js';
 
 console.log('Adding some stuff: ', addNumbers(1, 2));
+
+console.log('Subtracting some stuff: ', subtractNumbers(1, 2));
