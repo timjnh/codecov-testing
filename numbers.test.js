c8ebb5041b974a6f7bc5c2dedb6a0816e1d51ac8
Tim_Jones <tjones@aurorasolar.com>
2023-04-13 08:43:13 -0400

    Do they fail?

--- numbers.test.js
+++ numbers.test.js
@@ -2,6 +2,6 @@ import { addNumbers } from "./numbers.js";
 
 describe('addNumbers', () => {
   test("it should add two numbers together", () => {
-    expect(addNumbers(1, 2)).toBe(3);
+    expect(addNumbers(1, 2)).toBe(4);
   });
 });
